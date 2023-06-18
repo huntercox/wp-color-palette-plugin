c255f2fef69604dfa3b163b3d88f3594502917fa
Hunter Cox <huntersamuelcox1992@gmail.com>
2023-06-18 10:51:39 -0400

    Fixed the add color button so that you can add multiple colors, click save and it will update the database value with multiple colors and add it to existing palette. Also added a toggle switch to conditionally output the colors on the frontend.

--- assets/js/admin.js
+++ assets/js/admin.js
@@ -1,4 +1,24 @@
 jQuery(document).ready(function ($) {
-	$('.color-field').wpColorPicker();
-	console.log('did we make it here?');
+	function initColorPicker() {
+		$('.color-field:not(.wp-color-picker)').wpColorPicker({
+			change: function (e, ui) {
+				$(this).val(ui.color.toString());
+			}
+		});
+	}
+
+	initColorPicker();
+
+	$('#add-color').click(function (e) {
+		e.preventDefault();
+		var colorInput = $('<div><input type="text" name="hsc_color_palette[color][]" class="color-field" /><button class="remove-color">X</button></div></br>');
+		$(this).before(colorInput);
+		initColorPicker();
+	});
+
+	$(document).on('click', '.remove-color', function (e) {
+		e.preventDefault();
+		$(this).parent().next('br').remove();
+		$(this).parent().remove();
+	});
 });
--- src/ColorPalette/ColorPalette.php
+++ src/ColorPalette/ColorPalette.php
@@ -13,6 +13,9 @@ class ColorPalette
 		// Hook into the admin menu
 		add_action('admin_menu', [$this, 'add_page']);
 		add_action('admin_init', [$this, 'page_init']);
+
+
+		add_action('wp_head', [$this, 'output_palette_styles']);
 	}
 
 	public function enqueue_styles()
@@ -24,36 +27,6 @@ class ColorPalette
 	{
 		wp_enqueue_style('wp-color-picker');
 		wp_enqueue_script('color-palette-admin', MY_PLUGIN_DIR_URL . 'assets/js/admin.js', ['jquery', 'wp-color-picker'], '1.0.0', true);
-
-		$script = "
-	jQuery(document).ready(function($) {
-		function initColorPicker() {
-			$('.color-field').wpColorPicker({
-				change: function(e, ui) {
-					$(this).val(ui.color.toString());
-				}
-			});
-		}
-
-		initColorPicker();
-
-		$('#add-color').click(function(e) {
-			e.preventDefault();
-			var colorInput = $('<input type=\"text\" name=\"hsc_color_palette[color][]\" class=\"color-field\" /><button class=\"remove-color\">Remove</button><br />');
-			$(this).before(colorInput);
-			initColorPicker();
-		});
-
-		$(document).on('click', '.remove-color', function(e) {
-			e.preventDefault();
-			$(this).prev('.color-field').remove();
-			$(this).next('br').remove();
-			$(this).remove();
-		});
-	});
-	";
-
-		wp_add_inline_script('color-palette-admin', $script);
 	}
 
 	public function add_page()
@@ -80,13 +53,21 @@ class ColorPalette
 				<?php
 				// This prints out all hidden setting fields
 				if (isset($this->options['color'])) {
-					$color = $this->options['color'];
-					echo '<div style="width: 20px; height: 20px; background-color: ' . $color . ';"></div>';
+					$colors = $this->options['color'];
+					if (is_array($colors)) {
+						foreach ($colors as $color) {
+							echo '<div style="width: 20px; height: 20px; background-color: ' . $color . ';"></div>';
+						}
+					}
 				}
 				settings_fields('hsc_color_palette_group');
 				do_settings_sections('hsc_color_palette');
-				submit_button();
 				?>
+				<div id="color-pickers">
+					<button id="add-color">Add Color</button>
+					<?php
+					submit_button('Save Colors');
+					?>
 			</form>
 		</div>
 <?php
@@ -94,6 +75,7 @@ class ColorPalette
 
 	public function page_init()
 	{
+		// Choose Colors
 		register_setting(
 			'hsc_color_palette_group', // Option group
 			'hsc_color_palette', // Option name
@@ -114,18 +96,29 @@ class ColorPalette
 			'hsc_color_palette', // Page
 			'setting_section_id' // Section
 		);
+
+		// Toggle Palette
+		add_settings_field(
+			'toggle_palette', // ID
+			'Toggle Color Palette', // Title
+			[$this, 'toggle_palette_callback'], // Callback
+			'hsc_color_palette', // Page
+			'setting_section_id' // Section
+		);
 	}
 
 	public function sanitize($input)
 	{
 		$new_input = array();
-		if (isset($input['color']))
-			$new_input['color'] = sanitize_text_field($input['color']);
-
-
-
+		if (isset($input['color']) && is_array($input['color'])) {
+			foreach ($input['color'] as $color) {
+				$new_input['color'][] = sanitize_text_field($color);
+			}
+		}
+		$new_input['toggle_palette'] = isset($input['toggle_palette']) ? (bool) $input['toggle_palette'] : false;
 		return $new_input;
 	}
+
 
 	public function print_section_info()
 	{
@@ -134,14 +127,30 @@ class ColorPalette
 
 	public function color_callback()
 	{
-		printf(
-			'<input type="text" id="color" name="hsc_color_palette[color]" value="%s" class="color-field" />',
-			isset($this->options['color']) ? esc_attr($this->options['color']) : ''
-		);
+		$colors = isset($this->options['color']) ? $this->options['color'] : array('');
+		foreach ($colors as $index => $color) {
+			printf(
+				'<input type="text" id="color" name="hsc_color_palette[color][]" value="%s" class="color-field" /> <br/>',
+				esc_attr($color)
+			);
+		}
 	}
 
+	public function toggle_palette_callback()
+	{
+		$togglePalette = isset($this->options['toggle_palette']) ? $this->options['toggle_palette'] : false;
+		echo '<input type="checkbox" id="toggle_palette" name="hsc_color_palette[toggle_palette]" value="1" ' . checked(1, $togglePalette, false) . ' />';
+	}
 
-
-
-	// END CLASS
+	public function output_palette_styles()
+	{
+		$options = get_option('hsc_color_palette');
+		if (isset($options['toggle_palette']) && $options['toggle_palette'] && isset($options['color']) && is_array($options['color'])) {
+			echo '<style type="text/css">body {';
+			foreach ($options['color'] as $index => $color) {
+				echo "--color{$index}: {$color};";
+			}
+			echo '}</style>';
+		}
+	}
 }
