4a5fbbf1f49d19426b773ba0165635499420d932
Hunter Cox <huntersamuelcox1992@gmail.com>
2023-06-17 23:34:43 -0400

    Initial commit. Plugin activates successfully.

---
+++ README.txt
@@ -0,0 +1,10 @@
+=== Color Palette ===
+Contributors: Hunter Cox
+Requires at least: 5.5
+Tested up to: 5.8
+Stable tag: 1.0.0
+Requires PHP: 5.4
+License: GPLv2 or later
+License URI: http://www.gnu.org/licenses/gpl-2.0.html
+== Description ==
+Create a color palette via the WP Admin and output to frontend as CSS variables on the root of every document.
---
+++ hsc-color-palette.php
@@ -0,0 +1,22 @@
+<?php
+
+/**
+ * @package           hsc_color_palette
+ *
+ * @wordpress-plugin
+ * Plugin Name: HSC Color Palette
+ * Description: Create a color palette in WP Admin and output to the frontend of your site as CSS variables.
+ * Version: 1.0.0
+ * Author: Hunter Cox
+ * Author URI: www.huntercox.dev
+ * License: GPL-2.0+
+ * License URI: http://www.gnu.org/licenses/gpl-2.0.txt
+ * Text Domain: hsc-color-palette
+ */
+
+namespace hsc_color_palette;
+
+
+if (!defined('ABSPATH')) {
+	exit; // Exit if accessed directly
+}
