cde1eb6c26c7ad9cfae3d66ca80a5fbe689958f7
Hunter Cox <huntersamuelcox1992@gmail.com>
2023-06-19 09:51:12 -0400

    Add text field to new row when clicking Add Color.

--- assets/js/admin.js
+++ assets/js/admin.js
@@ -11,7 +11,7 @@ jQuery(document).ready(function ($) {
 
 	$('#add-color').click(function (e) {
 		e.preventDefault();
-		var colorInput = $('<div><input type="text" name="hsc_color_palette[color][]" class="color-field" /><button class="remove-color">X</button></div></br>');
+		var colorInput = $('<div><input type="text" name="hsc_color_palette[color][]" class="color-field" /><input type="text" name="hsc_color_palette[id][]" class="id-field" /><button class="remove-color">X</button></div></br>');
 		$(this).before(colorInput);
 		initColorPicker();
 	});
